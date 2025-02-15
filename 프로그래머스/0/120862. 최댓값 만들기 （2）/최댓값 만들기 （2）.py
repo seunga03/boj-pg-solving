6de5bc1661f9f67b2a1b5eb69d5acf4a4e470689
SeungA Lee <seungalee220@gmail.com>
2025-02-15 12:14:39 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,7 +1,3 @@
 def solution(numbers):
     numbers.sort()
-    
-    if numbers[0] < 0 and numbers[1] < 0:
-        return max(numbers[-1] * numbers[-2], numbers[0] * numbers[1])
-    
-    return numbers[-1] * numbers[-2]
+    return max(numbers[-1] * numbers[-2], numbers[0] * numbers[1])

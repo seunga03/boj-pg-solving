378fc95933071b4c3bac3c75d1db706a520d6e34
SeungA Lee <seungalee220@gmail.com>
2025-07-29 23:56:08 +0900

    [Bronze I] Title: 단어 공부, Time: 188 ms, Memory: 34368 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,19 +1,10 @@
 word = input().upper()
-l = list(set(word))
-tmp = [0] * len(l)
+cnt = [0] * 26
 
 for i in word:
-    for j in range(len(l)):
-        if i == l[j]:
-            tmp[j] += 1
-
-cnt = 0
-
-for k in tmp:
-    if max(tmp) == k:
-        cnt += 1
-
-if cnt == 1:
-    print(l[tmp.index(max(tmp))])
+    cnt[ord(i) - 65] += 1
+    
+if cnt.count(max(cnt)) >= 2:
+    print('?')
 else:
-    print("?")
+    print(chr(cnt.index(max(cnt)) + 65))

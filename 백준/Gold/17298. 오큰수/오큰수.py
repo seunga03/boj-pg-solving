7bceaab7b71b70aebbabe29a44ea2abbf81ae2d1
SeungA Lee <seungalee220@gmail.com>
2025-08-13 23:07:02 +0900

    [Gold IV] Title: 오큰수, Time: 1016 ms, Memory: 129196 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -0,0 +1,14 @@
+n = int(input())
+A = list(map(int, input().split()))
+stack = []
+answer = [0] * n
+
+for i in range(n):
+    while stack and A[stack[-1]] < A[i]:
+        answer[stack.pop()] = A[i]
+    stack.append(i)
+    
+while stack:
+    answer[stack.pop()] = -1
+    
+print(*answer)

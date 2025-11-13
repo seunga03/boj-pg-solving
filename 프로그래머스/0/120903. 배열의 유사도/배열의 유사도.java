18223f3c54175dbcc61263a32727792277712d66
Seung A Lee <seungalee220@gmail.com>
2025-11-13 13:24:44 +0900

    [level 0] Title: 배열의 유사도, Time: 0.16 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        Set<String> set = new HashSet<>(Arrays.asList(s2));
+        int answer = 0;
+
+        for (String s : s1) {
+            if (set.contains(s)) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}

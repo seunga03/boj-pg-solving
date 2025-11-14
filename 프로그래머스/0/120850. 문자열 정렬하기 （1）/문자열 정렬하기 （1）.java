8b767e974ac5c0f7af88ae0508b7a2f6d87a3830
Seung A Lee <seungalee220@gmail.com>
2025-11-14 10:33:01 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 2.87 ms, Memory: 87.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String my_string) {
+        List<Integer> list = new ArrayList<>();
+
+        for (char c : my_string.toCharArray()) {
+            if (Character.isDigit(c)) {
+                list.add(c - '0');
+            }
+        }
+
+        Collections.sort(list);
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
